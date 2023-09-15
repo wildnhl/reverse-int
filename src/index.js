25d94722d687a3067392610a96fa915a68b7b9eb
Aliaksei Yankouski <minnesotanhlwild@gmail.com>
2023-09-15 15:32:58 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+   const arr = (n + '').split('');
+   let finalArr = '';
+   for (let i = arr.length - 1; i >= 0; i--) {
+      finalArr = finalArr + arr[i];
+   }
+   return parseInt(finalArr);
+};
